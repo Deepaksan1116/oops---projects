51af98679837851828c6a38b1b502c701f4f6a69
Deepak <107839439+Deepaksan1116@users.noreply.github.com>
2023-04-03 12:23:50 +0530

    Add files via upload

---
+++ Simple_Electrical_Switch_OO/Simple_Electrical_Switch_Console_App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Electrical_Switch_OO;
+
+namespace Simple_Electrical_Switch_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Switch temp_switch = new Switch();
+            Console.WriteLine("Please Enter Your Input");
+            string input = Console.ReadLine();
+            string lower_case_input = input.ToLower();
+            if (Enum.IsDefined(typeof(Status), lower_case_input))
+            {
+                Status temp_status = (Status)Enum.Parse(typeof(Status), lower_case_input);
+                temp_switch.Switch_status = temp_status;
+            }
+            else
+            {
+                throw new Exception("Invalid input... Please Check the input");
+            }
+            temp_switch.Set_Switch_Status();
+            Console.WriteLine(temp_switch);
+        }
+    }
+}
---
+++ Simple_Electrical_Switch_OO/Simple_Electrical_Switch_OO/Switch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Electrical_Switch_OO
+{
+    public class Switch
+    {
+        Status _switch_status;
+
+        public Status Switch_status
+        {
+            get { return _switch_status; }
+            set { _switch_status =value; }
+        }
+
+        public void Set_Switch_Status()
+        {
+            if (Switch_status == Status.off)
+            {
+                Switch_status = Status.on;
+            }
+            else if (Switch_status==Status.on)
+            {
+                Switch_status = Status.off;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Switch Status is : {0}",this._switch_status);
+        }
+
+    }
+
+    public enum Status
+    { 
+        on,
+        off,
+    }
+    
+}
